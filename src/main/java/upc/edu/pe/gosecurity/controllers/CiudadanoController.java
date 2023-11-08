c2b34e78df4a8752ab6692308970beac27efd6a0
Arlet Aquije <128516094+U20201F777@users.noreply.github.com>
2023-11-08 09:27:42 -0500

    Limpieza de codigo

--- src/main/java/upc/edu/pe/gosecurity/controllers/CiudadanoController.java
+++ src/main/java/upc/edu/pe/gosecurity/controllers/CiudadanoController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("ciudadano")
+@RequestMapping("/ciudadano")
 public class CiudadanoController {
 
     @Autowired
--- src/main/java/upc/edu/pe/gosecurity/controllers/PoliciaController.java
+++ src/main/java/upc/edu/pe/gosecurity/controllers/PoliciaController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("policia")
+@RequestMapping("/policia")
 public class PoliciaController {
 
     @Autowired
