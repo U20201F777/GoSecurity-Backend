41f5000ae254b9f239e7d06a65f6a2a74c7bb6fd
Arlet <128516094+U20201F777@users.noreply.github.com>
2023-11-18 17:41:52 -0500

    Query - Arlet

--- src/main/java/upc/edu/pe/gosecurity/repositories/IPoliciaRepository.java
+++ src/main/java/upc/edu/pe/gosecurity/repositories/IPoliciaRepository.java
@@ -12,24 +12,22 @@ public interface IPoliciaRepository extends JpaRepository<Policia,Integer> {
     List<Policia> findByNumeroPlacaPolicia(String NroPlaca);
     @Query("from Policia v where v.users.username =:username")
     Policia listByUser(String username);
-    @Query(value = "SELECT \n" +
-            "    C.numero_ciudadano AS DNI_Ciudadano,\n" +
-            "    P.nombre_p AS Nombre_pertenencia,\n" +
-            "    COUNT(CASE WHEN K.tipo_caso = 'atendido' THEN P.id_pertenencias ELSE NULL END) AS Cant_Atendido,\n" +
-            "    COUNT(CASE WHEN K.tipo_caso = 'no atendido' THEN P.id_pertenencias ELSE NULL END) AS Cant_No_Atendido\n" +
-            "FROM \n" +
-            "    tipo_caso AS K\n" +
-            "LEFT JOIN \n" +
-            "    notificacion AS G ON K.id_tipo_caso = G.id_tipo_caso\n" +
-            "LEFT JOIN \n" +
-            "    ayuda AS A ON G.id_tipo_caso = A.id_tipo_caso\n" +
-            "INNER JOIN \n" +
-            "    pertenencias AS P ON A.id_ayuda = P.id_ayuda\n" +
-            "INNER JOIN \n" +
-            "    ciudadano AS C ON P.id_ciudadano = C.id_ciudadano\n" +
-            "WHERE \n" +
-            "    K.tipo_caso IN ('atendido', 'no atendido')\n" +
-            "GROUP BY \n" +
-            "    C.numero_ciudadano, P.nombre_p;", nativeQuery = true)
+    @Query(value = "SELECT C.numero_ciudadano AS DNI_Ciudadano,\n" +
+            "       COUNT(CASE WHEN K.tipo_caso = 'atendido' THEN P.id_pertenencias ELSE NULL END) AS Cant_Atendido,\n" +
+            "       COUNT(CASE WHEN K.tipo_caso = 'no atendido' THEN P.id_pertenencias ELSE NULL END) AS Cant_No_Atendido\n" +
+            "       FROM\n" +
+            "       tipo_caso AS K\n" +
+            "       LEFT JOIN\n" +
+            "       notificacion AS G ON K.id_tipo_caso = G.id_tipo_caso\n" +
+            "       LEFT JOIN\n" +
+            "       ayuda AS A ON G.id_tipo_caso = A.id_tipo_caso\n" +
+            "       INNER JOIN\n" +
+            "       pertenencias AS P ON A.id_ayuda = P.id_ayuda\n" +
+            "       INNER JOIN\n" +
+            "       ciudadano AS C ON P.id_ciudadano = C.id_ciudadano\n" +
+            "       WHERE\n" +
+            "       K.tipo_caso IN ('atendido', 'no atendido')\n" +
+            "       GROUP BY\n" +
+            "       C.numero_ciudadano;", nativeQuery = true)
     public List<String[]> casosPorTipo();
 }
